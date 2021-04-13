1fe4f1c3b4533d9b0ab203d3ee2e52783f8bb7cf
Jonathan Otto <jonootto@gmail.com>
2021-04-13 17:47:43 +1200

    concat

--- src/influxstat.py
+++ src/influxstat.py
@@ -35,7 +35,7 @@ def hashrate(rates,total):
     print(str(len(rates)) + " CARDS")
     json_body_rates = ""
     for x in range(len(rates)):
-        json_body_rates = json_body_rates + [
+        json_body_rates += [
         {
            "measurement": "hashrate",
             "tags": {
