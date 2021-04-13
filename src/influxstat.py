afc812e71962e014f5765994544127db85a17439
Jonathan Otto <jonootto@gmail.com>
2021-04-13 17:52:18 +1200

    type

--- src/influxstat.py
+++ src/influxstat.py
@@ -33,7 +33,7 @@ def toinflux(input):
 
 def hashrate(rates,total):
     print(str(len(rates)) + " CARDS")
-    json_body_rates = ""
+    json_body_rates = str("")
     for x in range(len(rates)):
         json_body_rates += [
         {
